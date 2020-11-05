7070b317801aa11c5343972be631eeac3e9c11ec
unknown <josedanielgrn01@gmail.com>
2020-11-05 17:22:44 -0600

    cifrado ruta

--- LibreriaGenericos/Clases/Ruta.cs
+++ LibreriaGenericos/Clases/Ruta.cs
@@ -1,20 +1,132 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace LibreriaGenericos.Clases
 {
    public class Ruta : Cifrado
     {
+        int Altura, Ancho;
+        public void Encriptar(string RutaOriginal, string RutaDestino, int altura, int ancho)
+        {
+            Altura = altura;
+            Ancho = ancho;
+            Cifrar(RutaOriginal, RutaDestino);
+        }
+        public void Desencriptar(string RutaOriginal, string RutaDestino, int altura, int ancho)
+        {
+            Altura = altura;
+            Ancho = ancho;
+            Decifrar(RutaOriginal, RutaDestino);
+        }
 
         protected override void Cifrar(string RutaOriginal, string RutaDestino)
         {
-            throw new NotImplementedException();
+            //establecer rutas de archivos y buffer
+            FileStream LecturaOriginal = new FileStream(RutaOriginal, FileMode.OpenOrCreate);
+            using var Reader = new BinaryReader(LecturaOriginal);
+            var buffer = new byte[2000000];
+
+            //creacion lista de arreglos, variables
+            List<byte[,]> ListaArreglos = new List<byte[,]>();
+            List<byte> ListaBytes = new List<byte>();
+            int ContadorBuffer = 0;
+
+            //creacion writer
+            using var Writer = new FileStream(RutaDestino, FileMode.OpenOrCreate);
+
+            //obtencion lista arreglos
+            while (Reader.BaseStream.Position != Reader.BaseStream.Length)
+            {
+                buffer = Reader.ReadBytes(Altura * Ancho);
+                byte[,] ArregloTemporal = new byte[Altura, Ancho];
+                for (int i = 0; i < Altura; i++)
+                {
+                    for (int j = 0; j < Ancho; j++)
+                    {
+                        if (ContadorBuffer < buffer.Length)
+                        {
+                            ArregloTemporal[j, i] = buffer[ContadorBuffer];
+                            ++ContadorBuffer;
+                        }
+                        else
+                        {
+                            ArregloTemporal[j, i] = 158;
+                        }
+                    }
+                }
+                ListaArreglos.Add(ArregloTemporal);
+                ContadorBuffer = 0;
+            }
+
+            //impresion arreglos en lista
+            foreach (var item in ListaArreglos)
+            {
+                for (int i = 0; i < Altura; i++)
+                {
+                    for (int j = 0; j < Ancho; j++)
+                    {
+                        ListaBytes.Add(item[i, j]);
+                    }
+                }
+                Writer.Write(ListaBytes.ToArray(), 0, ListaBytes.ToArray().Length);
+                ListaBytes.Clear();
+            }
+            Writer.Close();
+            Reader.Close();
         }
 
         protected override void Decifrar(string RutaOriginal, string RutaDestino)
         {
-            throw new NotImplementedException();
+            //establecer rutas de archivos y buffer
+            FileStream LecturaOriginal = new FileStream(RutaOriginal, FileMode.OpenOrCreate);
+            using var Reader = new BinaryReader(LecturaOriginal);
+            var buffer = new byte[2000000];
+
+            //creacion lista de arreglos, variables
+            List<byte[,]> ListaArreglos = new List<byte[,]>();
+            List<byte> ListaBytes = new List<byte>();
+            int ContadorBuffer = 0;
+
+            //creacion writer
+            using var Writer = new FileStream(RutaDestino, FileMode.OpenOrCreate);
+
+            //obtencion lista arreglos
+            while (Reader.BaseStream.Position != Reader.BaseStream.Length)
+            {
+                buffer = Reader.ReadBytes(Altura * Ancho);
+                byte[,] ArregloTemporal = new byte[Altura, Ancho];
+                for (int i = 0; i < Altura; i++)
+                {
+                    for (int j = 0; j < Ancho; j++)
+                    {
+                        ArregloTemporal[i, j] = buffer[ContadorBuffer];
+                        ++ContadorBuffer;
+                    }
+                }
+                ListaArreglos.Add(ArregloTemporal);
+                ContadorBuffer = 0;
+            }
+
+            //impresion arreglos en lista
+            foreach (var item in ListaArreglos)
+            {
+                for (int i = 0; i < Altura; i++)
+                {
+                    for (int j = 0; j < Ancho; j++)
+                    {
+                        if (item[j, i] != 158)
+                        {
+                            ListaBytes.Add(item[j, i]);
+                        }
+                    }
+                }
+                Writer.Write(ListaBytes.ToArray(), 0, ListaBytes.ToArray().Length);
+                ListaBytes.Clear();
+            }
+            Writer.Close();
+            Reader.Close();
         }
     }
 }
--- PruebaConsola/Program.cs
+++ PruebaConsola/Program.cs
@@ -11,23 +11,13 @@ namespace PruebaConsola
     {
         static void Main(string[] args)
         {
-            //string Ruta = Path.GetFullPath("..\\..\\..\\..\\API_Proyecto3\\Archivos");
-            string Path1 = @"C:\Users\Crist\Desktop\LAB05_ED02\API\Archivos\Archivo1.txt";
-            string Path2 = @"C:\Users\Crist\Desktop\LAB05_ED02\API\Archivos\Archivo2.txt";
-            string RutaTexto = @"C:\Users\Crist\Desktop\Cifrado\API_Proyecto3\ArchivosPrueba\BIBLIA COMPLETA.txt";
-            // string RutaTexto = @"C:\Users\Crist\Desktop\Cifrado\API_Proyecto3\ArchivosPrueba\la odisea.txt";
-            //string RutaTexto = @"C:\Users\Crist\Desktop\Cifrado\API_Proyecto3\ArchivosPrueba\easy test.txt";
-            //string RutaTexto = @"C:\Users\Crist\Desktop\Cifrado\API_Proyecto3\ArchivosPrueba\Prueba1.txt";
-            //Cesar CifradoCesar = new Cesar();
-            //CifradoCesar.Encriptar(RutaTexto, Path1, "murcielago");
-            //CifradoCesar.Desencriptar(Path1, Path2, "murcielago");
+            string Path1 = @"C:\Users\josed\Desktop\LAB05_ED02\API\Archivos\Archivo1.txt";
+            string Path2 = @"C:\Users\josed\Desktop\LAB05_ED02\API\Archivos\Archivo2.txt";
+            string RutaTexto = @"C:\\Users\\josed\\Desktop\\biblia.txt";
             ZigZag Cifradozigzag = new ZigZag();
-            Cesar cesar = new Cesar();
-            AuxClase auxClase = new AuxClase();
-            //auxClase.Encode(RutaTexto, Path1, 6);
-            //auxClase.Decode(Path1, Path2, 6);
-            cesar.Encriptar(RutaTexto, Path1, "Parangaracutirimicuaro");
-            cesar.Desencriptar(Path1, Path2, "parangaracutirimicuaro");
+            Ruta cesar = new Ruta();
+            cesar.Encriptar(RutaTexto, Path1, 6,6);
+            cesar.Desencriptar(Path1, Path2, 6,6);
 
 
         }
